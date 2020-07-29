fcd23d5c2598199b9c6dd2689cdc352066823257
mnfera <mikaelnilton@hotmail.com>
2020-07-29 00:23:44 -0300

    Criando páginas, navegação e personalizações

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { FooterBase } from './styles';
+import LogoFooter from '../../assets/img/linkedin.png';
 
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      <a href="https://www.linkedin.com/in/mikael-nilton/">
+        <img className="Logo" src={LogoFooter} alt="Logo Linkedin" />
       </a>
       <p>
         Orgulhosamente criado durante a
---
+++ src/pages/cadastro/Categoria/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PageDefault from '../../../components/PageDefault';
+import { Link } from 'react-router-dom';
+
+function CadastroCategoria() {
+    return (
+        <PageDefault>
+            <h1>Categorias</h1>
+
+            <form>
+
+                <label>
+                    Nome da Categoria:
+                <input
+                    type="text"
+                />
+                </label>
+
+                <button>
+                    Cadastrar
+                </button>
+            </form>
+
+            <Link to="/">
+                Home
+            </Link>
+        </PageDefault>
+    );
+}
+
+export default CadastroCategoria;
